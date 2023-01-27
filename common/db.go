78de9ba19868788c91a330bd5a0b69ab7f30706d
alisalim17 <alisalimli1899@gmail.com>
2023-01-27 19:33:04 +0400

    gorm logger

--- common/db.go
+++ common/db.go
@@ -1,6 +1,10 @@
 package common
 
 import (
+	"log"
+	"os"
+	"time"
+
 	"github.com/elisalimli/meetmeup/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,9 +17,19 @@ var tables []interface{} = []interface{}{
 }
 
 func InitDb() (*gorm.DB, error) {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  true,        // Disable color
+		},
+	)
+
 	var err error
 	db, err := gorm.Open(postgres.Open("postgres://postgres:postgres@localhost:5432/abc?sslmode=disable"), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Silent),
+		Logger: newLogger,
 	})
 	if err != nil {
 		return nil, err
